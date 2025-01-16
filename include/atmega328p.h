f7d7905a603094b3e790b3ef04d7526c6c500b02
Rodrigo <rodrigof0501@gmail.com>
2025-01-16 16:52:23 -0300

    Start project

---
+++ include/atmega328p.h
@@ -0,0 +1,14 @@
+// ----------- ATmega328p Registers Config. ---------------
+#include <avr/io.h>
+
+// ---------------- Interrupts handling -----------------
+
+void enable_interruptions(void) {
+    SREG |= (1 << 7);  // SET Bit-7 of SREG 
+}
+
+void disable_interruptions(void) {
+    SREG &= ~(1 << 7); // CLEAR Bit-7 of SREG                  
+}
+
+//
