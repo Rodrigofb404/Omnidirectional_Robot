49e454bc187a7cfd297f9a109ce0ab589dd2e3d6
Rodrigo <rodrigof0501@gmail.com>
2025-01-16 22:26:48 -0300

    Delete interrupts functions

--- include/atmega328p.h
+++ include/atmega328p.h
@@ -1,14 +1,5 @@
-// ----------- ATmega328p Registers Config. ---------------
 #include <avr/io.h>
 #include <util/delay.h>
-// ---------------- Interrupts handling -----------------
 
-void enable_interruptions(void) {
-    SREG |= (1 << 7);  // SET Bit-7 of SREG 
-}
-
-void disable_interruptions(void) {
-    SREG &= ~(1 << 7); // CLEAR Bit-7 of SREG                  
-}
-
-//
+#define SET_ALL 0xFF
+#define CLEAR_ALL 0x00
