71588372c121fb9366d054f6254525a15c165681
Rodrigo Freitas <rodrigof0501@gmail.com>
2025-02-01 01:58:02 -0300

    Fix IO

--- include/io.h
+++ include/io.h
@@ -1,19 +1,19 @@
 #include <avr/io.h>
 
-#define SET_FIRST_5_BITS 0b00011111
+#define FIRST_5_BITS 0b00011111
+#define PC0_PC1_PC2_PC3 0b00001111
 // ======================================================================
 // Configura os pinos utilizados para os encoders
 // ======================================================================
 void encoders_io_config () {
-    // PB0 (PCINT0), PC0 (PCINT8), PD7 (PCINT23) as inputs
+
     DDRB &= ~((1 << DDB0));
-    DDRC &= ~((1 << DDC0));
+    DDRC &= ~(PC0_PC1_PC2_PC3);  // PC0...PC3 -> INPUT
     DDRD &= ~((1 << DDD7));
 
     PORTB &= ~((1 << PORTB0)); 
-    PORTC &= ~((1 << PORTC0));
-    PORTD &= ~((1 << PORTD7));
-
+    PORTD &= ~((1 << PORTD7)); 
+    PORTC &= ~(PC0_PC1_PC2_PC3);
 }
 
 // ======================================================================
@@ -25,18 +25,19 @@ void timer_io_config() {
     DDRD |= (1 << DDD6); // PD6
     DDRD |= (1 << DDD5); // PD5
 
-    // Timer 1
-    DDRB |= (1 << DDB1); // PB1
-    DDRB |= (1 << DDB2); // PB2
+    // // Timer 1
+    // DDRB |= (1 << DDB1); // PB1
+    // DDRB |= (1 << DDB2); // PB2
 
     // Timer 2 REVISAR
-    // DDRB |= (1 << DDB3); // PB3
-    // DDRD |= (1 << DDD3); // PD3
+    DDRB |= (1 << DDB3); // PB3
+    DDRD |= (1 << DDD3); // PD3
 }
 
 void h_bridge_config() {
-    DDRD = SET_FIRST_5_BITS; // PD0...PD5 -> OUTPUT
-}
+    DDRD = 0b00110111; // PD0...PD5 -> OUTPUT
+    DDRB |= (1 << PB1);
+}   
 
 void IO_init() {
     h_bridge_config();
